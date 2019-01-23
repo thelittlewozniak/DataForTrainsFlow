7eb1a30755f084e1aab6447c53965b4f57230b14
Nathan Pire <35301322+thelittlewozniak@users.noreply.github.com>
2019-01-23 21:38:43 +0100

    Api weather classes created

---
+++ DataForTrainsFlow/DataForTrainsFlow/APIWeather/Temperature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataWeatherForTrainsFlow.APIWeather
+{
+    class Temperature
+    {
+        public Metric metric { get; set; }
+    }
+}
---
+++ DataForTrainsFlow/DataForTrainsFlow/APIWeather/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataWeatherForTrainsFlow.APIWeather
+{
+    class Weather
+    {
+        public string WeatherText { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public string PrecipitationType { get; set; }
+        public Temperature Temperature { get; set; }
+        public double RelativeHumidity { get; set; }
+    }
+}
