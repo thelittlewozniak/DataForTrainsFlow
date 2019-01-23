1520fff7745a0250dcbdfd194d1e432720716d4f
Nathan Pire <35301322+thelittlewozniak@users.noreply.github.com>
2019-01-23 21:29:34 +0100

    creation of the API and the library class

---
+++ DataForTrainsFlow/ClassLibrary1/Poco/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonClasses.Poco
+{
+    class Weather
+    {
+        public int Id { get; set; }
+        public string WeatherText { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public string PrecipitationType { get; set; }
+        public int RelativeHumidity { get; set; }
+    }
+}
