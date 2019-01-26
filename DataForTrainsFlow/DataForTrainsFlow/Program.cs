a8e77eaf7a4fb32865bca0180b14318e626e56cb
Nathan Pire <35301322+thelittlewozniak@users.noreply.github.com>
2019-01-26 15:51:24 +0100

    editing the automated application

--- DataForTrainsFlow/DataForTrainsFlow/Program.cs
+++ DataForTrainsFlow/DataForTrainsFlow/Program.cs
@@ -1,4 +1,5 @@
-﻿using DataWeatherForTrainsFlow.APIWeather;
+﻿using DataWeatherForTrainsFlow;
+using DataWeatherForTrainsFlow.APIWeather;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -60,27 +61,7 @@ namespace DataForTrainsFlow
                 {
                     if(now.Hour==tab[i].Hour && now.Minute == tab[i].Minute)
                     {
-                        Console.WriteLine("*********************************************************************");
-                        Console.WriteLine("Begin the analyze time:"+now.ToString());
-                        try
-                        {
-                            string url = "http://dataservice.accuweather.com/currentconditions/v1/28244?apikey=W9GYs6vGJ9SJGIKv2EM7hMeHSnw6xu9C";
-                            var json = webClient.DownloadString(url);
-                            var dataAPI = JsonConvert.DeserializeObject<List<Weather>>(json);
-                            Console.WriteLine("Adding into the database");
-                            Console.WriteLine("Weather:" + dataAPI[0].WeatherText);
-                            Console.WriteLine("Precipitaion:" + dataAPI[0].HasPrecipitation);
-                            Console.WriteLine("Temperature:" + dataAPI[0].Temperature.metric.Value);
-                            string addingData = "http://weathertrainsflow.azurewebsites.net/api/Weather/Add?weatherText=" + dataAPI[0].WeatherText + "&hasPrecipitation=" + dataAPI[0].HasPrecipitation + "&precipitationType=" + dataAPI[0].PrecipitationType + "&relativeHumidity=" + dataAPI[0].RelativeHumidity + "&temperature=" + (int)dataAPI[0].Temperature.metric.Value + "&dateTime=" + now.ToString("MM/dd/yyyy HH:mm");
-                            json = new WebClient().DownloadString(addingData);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine("Error(s):"+e.Message);
-                        }
-                        System.Threading.Thread.Sleep(60000);
-                        Console.WriteLine("End the analyze time:" + now.ToString());
-                        Console.WriteLine("*********************************************************************");
+                        new WeatherAnalyse(now).Analyse();
                     }
                 }
             }
